bd76bc5f6dd454a06688be7d292fcac9b5c08cca
Sebastian Balanta Salazar <sebastian.balanta@novaip.com.co>
2019-11-04 14:18:48 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { IngresoEgresoComponent } from './components/ingreso-egreso/ingreso-egreso.component';
+import { EstadisticaComponent } from './components/ingreso-egreso/estadistica/estadistica.component';
+import { DetalleComponent } from './components/ingreso-egreso/detalle/detalle.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    IngresoEgresoComponent,
+    EstadisticaComponent,
+    DetalleComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/dashboard/dashboard.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
+import { IngresoEgresoComponent } from '../ingreso-egreso/ingreso-egreso.component';
+import { EstadisticaComponent } from '../ingreso-egreso/estadistica/estadistica.component';
+import { DetalleComponent } from '../ingreso-egreso/detalle/detalle.component';
+
+
+
+export const dashboardRoutes: Routes = [
+    { path: '', component: EstadisticaComponent },
+    { path: 'ingreso-egreso', component: IngresoEgresoComponent },
+    { path: 'detalle', component: DetalleComponent },
+];
